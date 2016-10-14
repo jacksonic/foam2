5c95d67601c65f32936107761cb4a61e7e9e2a7b
Adam Van Ymeren <adamvy@google.com>
2016-10-14 13:17:54 -0400

    Add pubsub support to FObject.
    Summary:
      All FObject instances gain generic high-performance publish and subscribe support.
      This can be used for any events, but will specifically be used for property change events in the future.
    
    Test Plan:
      100% code coverage
    
    Reviewers: anicolao
    
    Reviewed By: anicolao
    
    Tags: #foam
    
    Differential Revision: http://codereview.cc/D1394

--- src/core/FObject.js
+++ src/core/FObject.js
@@ -89,6 +89,237 @@ foam.CLASS({
       }
     },
 
+    /************************************************
+     * Publish and Subscribe
+     ************************************************/
+
+    /**
+      This structure represents the head of a doubly-linked list of
+      listeners. It contains 'next', a pointer to the first listener,
+      and 'children', a map of sub-topic chains.
+
+      Nodes in the list contain 'next' and 'prev' links, which lets
+      removing subscriptions be done quickly by connecting next to prev
+      and prev to next.
+
+      Note that both the head structure and the nodes themselves have a
+      'next' property. This simplifies the code because there is no
+      special case for handling when the list is empty.
+
+      Listener List Structure
+      -----------------------
+      next     -> {
+        prev: <-,
+        sub: {src: <source object>, destroy: <destructor function> },
+        l: <listener>,
+        next: -> },
+      children -> {
+          subTopic1: <same structure>,
+          ...
+          subTopicn: <same structure>
+      }
+
+      TODO: Move this structure to a foam.LIB, and add a benchmark
+      to show why we are using plain javascript objects rather than
+      modeled objects for this structure.
+    */
+    function createListenerList_() {
+      return { next: null };
+    },
+
+    /** Return the top-level listener list, creating if necessary. */
+    function listeners_() {
+      return this.getPrivate_('listeners') ||
+        this.setPrivate_('listeners', this.createListenerList_());
+    },
+
+    /**
+      Notify all of the listeners in a listener list.
+      Pass 'a' arguments to listeners.
+      Returns the number of listeners notified.
+    */
+    function notify_(listeners, a) {
+      var count = 0;
+      while ( listeners ) {
+        var l = listeners.l;
+        var s = listeners.sub;
+
+        // Update 'listeners' before notifying because the listener
+        // may set next to null.
+        listeners = listeners.next;
+
+        // Like l.apply(l, [s].concat(Array.from(a))), but faster.
+        // FUTURE: add benchmark to justify
+        // ???: optional exception trapping, benchmark
+        switch ( a.length ) {
+          case 0: l(s); break;
+          case 1: l(s, a[0]); break;
+          case 2: l(s, a[0], a[1]); break;
+          case 3: l(s, a[0], a[1], a[2]); break;
+          case 4: l(s, a[0], a[1], a[2], a[3]); break;
+          case 5: l(s, a[0], a[1], a[2], a[3], a[4]); break;
+          case 6: l(s, a[0], a[1], a[2], a[3], a[4], a[5]); break;
+          case 7: l(s, a[0], a[1], a[2], a[3], a[4], a[5], a[6]); break;
+          case 8: l(s, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]); break;
+          case 9: l(s, a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]); break;
+          default: l.apply(l, [ s ].concat(Array.from(a)));
+        }
+        count++;
+      }
+      return count;
+    },
+
+    function hasListeners(/* args */) {
+      /** Return true iff there are listeners for the supplied message. **/
+      var listeners = this.getPrivate_('listeners');
+
+      for ( var i = 0 ; listeners ; i++ ) {
+        if ( listeners.next ) return true;
+        if ( i === arguments.length ) return false;
+        listeners = listeners.children && listeners.children[arguments[i]];
+      }
+
+      return false;
+    },
+
+    /**
+      Publish a message to all matching sub()'ed listeners.
+
+      All sub()'ed listeners whose specified pattern match the
+      pub()'ed arguments will be notified.
+      Ex.:
+<pre>
+  var obj  = foam.core.FObject.create();
+  var sub1 = obj.sub(               function(a,b,c) { console.log(a,b,c); });
+  var sub2 = obj.sub('alarm',       function(a,b,c) { console.log(a,b,c); });
+  var sub3 = obj.sub('alarm', 'on', function(a,b,c) { console.log(a,b,c); });
+
+  obj.pub('alarm', 'on');  // notifies sub1, sub2 and sub3
+  obj.pub('alarm', 'off'); // notifies sub1 and sub2
+  obj.pub();               // only notifies sub1
+  obj.pub('foobar');       // only notifies sub1
+</pre>
+
+      Note how FObjects can be used as generic pub/subs.
+
+      Returns the number of listeners notified.
+    */
+    function pub(a1, a2, a3, a4, a5, a6, a7, a8, a9) {
+      // This method prevents this function not being JIT-ed because
+      // of the use of 'arguments'. Doesn't generate any garbage ([]'s
+      // don't appear to be garbage in V8).
+      // FUTURE: benchmark
+      switch ( arguments.length ) {
+        case 0:  return this.pub_([]);
+        case 1:  return this.pub_([ a1 ]);
+        case 2:  return this.pub_([ a1, a2 ]);
+        case 3:  return this.pub_([ a1, a2, a3 ]);
+        case 4:  return this.pub_([ a1, a2, a3, a4 ]);
+        case 5:  return this.pub_([ a1, a2, a3, a4, a5 ]);
+        case 6:  return this.pub_([ a1, a2, a3, a4, a5, a6 ]);
+        case 7:  return this.pub_([ a1, a2, a3, a4, a5, a6, a7 ]);
+        case 8:  return this.pub_([ a1, a2, a3, a4, a5, a6, a7, a8 ]);
+        case 9:  return this.pub_([ a1, a2, a3, a4, a5, a6, a7, a8, a9 ]);
+        default: return this.pub_(arguments);
+      }
+    },
+
+    function pub_(args) {
+      /** Internal publish method, called by pub(). */
+
+      // No listeners, so return.
+      if ( ! this.hasOwnPrivate_('listeners') ) return 0;
+
+      var listeners = this.listeners_();
+
+      // Notify all global listeners.
+      var count = this.notify_(listeners.next, args);
+
+      // Walk the arguments, notifying more specific listeners.
+      for ( var i = 0 ; i < args.length; i++ ) {
+        var listeners = listeners.children && listeners.children[args[i]];
+        if ( ! listeners ) break;
+        count += this.notify_(listeners.next, args);
+      }
+
+      return count;
+    },
+
+    /**
+      Subscribe to pub()'ed events.
+      args - zero or more values which specify the pattern of pub()'ed
+             events to match.
+      <p>For example:
+<pre>
+   sub('propertyChange', l) will match:
+   pub('propertyChange', 'age', 18, 19), but not:
+   pub('stateChange', 'active');
+</pre>
+      <p>sub(l) will match all events.
+      l - the listener to call with notifications.
+       <p> The first argument supplied to the listener is the "subscription",
+        which contains the "src" of the event and a destroy() method for
+        cancelling the subscription.
+      <p>Returns a "subscrition" which can be cancelled by calling
+        its .destroy() method.
+    */
+    function sub() { /* args..., l */
+      var l = arguments[arguments.length - 1];
+
+      console.assert(typeof l === 'function', 'Listener must be a function');
+
+      var listeners = this.listeners_();
+
+      for ( var i = 0 ; i < arguments.length - 1 ; i++ ) {
+        var children = listeners.children || ( listeners.children = {} );
+        listeners = children[arguments[i]] ||
+            ( children[arguments[i]] = this.createListenerList_() );
+      }
+
+      var node = {
+        sub:  { src: this },
+        next: listeners.next,
+        prev: listeners,
+        l:    l
+      };
+      node.sub.destroy = function() {
+        if ( node.next ) node.next.prev = node.prev;
+        if ( node.prev ) node.prev.next = node.next;
+
+        // Disconnect so that calling destroy more than once is harmless
+        node.next = node.prev = null;
+      };
+
+      if ( listeners.next ) listeners.next.prev = node;
+      listeners.next = node;
+
+      return node.sub;
+    },
+
+    /**
+      Unsub a previously sub()'ed listener.
+      It is more efficient to unsubscribe by calling .destroy()
+      on the subscription returned from sub() (so prefer that
+      method when possible).
+    */
+    function unsub() { /* args..., l */
+      var l         = arguments[arguments.length - 1];
+      var listeners = this.getPrivate_('listeners');
+
+      for ( var i = 0 ; i < arguments.length - 1 && listeners ; i++ ) {
+        listeners = listeners.children && listeners.children[arguments[i]];
+      }
+
+      var node = listeners && listeners.next;
+      while ( node ) {
+        if ( node.l === l ) {
+          node.sub.destroy();
+          return;
+        }
+        node = node.next;
+      }
+    },
+
     /** Create a deep copy of this object. **/
     function clone() {
       var m = {};
--- test/src/core/FObject.js
+++ test/src/core/FObject.js
@@ -175,4 +175,306 @@ describe('FObject features', function() {
     var obj = SomeClass.create();
     obj.testPrivates();
   });
+
+  it('pub/sub support', function() {
+    foam.CLASS({
+      name: 'SomeClass'
+    });
+
+    var obj = SomeClass.create();
+
+    var wasCalled1 = false;
+    var wasCalled2 = false;
+    var wasCalled3 = false;
+    var count;
+
+    count = obj.pub();
+    expect(count).toBe(0);
+
+    var s1 = obj.sub('some', 'topic', function(s, a, b, c) {
+      wasCalled1 = true;
+      expect(a).toBe('some');
+      expect(b).toBe('topic');
+      expect(c).toBe('value');
+
+      // No harm calling destroy multiple times.
+      s.destroy();
+      s.destroy();
+    });
+
+    expect(obj.hasListeners('some')).toBe(false);
+    expect(obj.hasListeners('some', 'topic')).toBe(true);
+    expect(obj.hasListeners('some', 'topic', '123')).toBe(true);
+    expect(obj.hasListeners('something', 'else')).toBe(false);
+
+    var s2 = obj.sub('some', 'topic', function(s, a, b, c) {
+      wasCalled2 = true;
+      expect(a).toBe('some');
+      expect(b).toBe('topic');
+    });
+
+    var listener3 = function(s, a, b, c) {
+      wasCalled3 = true;
+      expect(a).toBe('some');
+    };
+
+    var s3 = obj.sub('some', listener3);
+
+    count = obj.pub('some', 'topic', 'value');
+
+    expect(count).toBe(3);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(true);
+    expect(wasCalled3).toBe(true);
+
+    wasCalled1 = wasCalled2 = wasCalled3 = false;
+
+    count = obj.pub('some', 'topic');
+
+    // First listener destoryed itself via s.destroy();
+    expect(wasCalled1).toBe(false);
+
+    expect(wasCalled2).toBe(true);
+    expect(wasCalled3).toBe(true);
+    expect(count).toBe(2);
+
+    wasCalled1 = wasCalled2 = wasCalled3 = false;
+
+    count = obj.pub('some', 'othertopic', 'foo', 'asdfasdf');
+
+    expect(wasCalled1).toBe(false);
+
+    // Second listener doesn't match the 'some', 'othertopic'
+    expect(wasCalled2).toBe(false);
+
+    expect(wasCalled3).toBe(true);
+    expect(count).toBe(1);
+
+    // Can unsubscribe by destroying the returned object
+    s2.destroy();
+
+    wasCalled1 = wasCalled2 = wasCalled3 = false;
+
+    count = obj.pub('some', 'topic');
+
+    expect(wasCalled1).toBe(false);
+
+    // Listener 2 has been unsubscribed via s2.destroy()
+    expect(wasCalled2).toBe(false);
+    expect(wasCalled3).toBe(true);
+
+
+    // Can also unsubscribe via unsub().
+    obj.unsub('some', listener3);
+
+    wasCalled1 = wasCalled2 = wasCalled3 = false;
+
+    count = obj.pub('some', 'topic');
+    expect(wasCalled1).toBe(false);
+    expect(wasCalled2).toBe(false);
+    expect(wasCalled3).toBe(false);
+    expect(count).toBe(0);
+  });
+
+  // Provides coverage of all cases when a subscription is
+  // removed from the linked list.
+  it('pub/sub unsubscribing code coverage', function() {
+    foam.CLASS({
+      name: 'SomeClass'
+    });
+
+    var obj = SomeClass.create();
+
+    var wasCalled1 = false;
+    var wasCalled2 = false;
+    var wasCalled3 = false;
+    var wasCalled4 = false;
+    var wasCalled5 = false;
+
+    var listener1 = function() { wasCalled1 = true; };
+    var listener2 = function() { wasCalled2 = true; };
+    var listener3 = function() { wasCalled3 = true; };
+    var listener4 = function() { wasCalled4 = true; };
+    var listener5 = function() { wasCalled5 = true; };
+
+    var s1 = obj.sub('a', 'b', 'c', listener1);
+    var s2 = obj.sub('a', 'b', 'c', listener2);
+    var s3 = obj.sub('a', 'b', 'c', listener3);
+    var s4 = obj.sub('a', 'b', 'c', listener4);
+    var s5 = obj.sub('a', 'b', 'c', listener5);
+
+
+    // nodes in the list are in reverse order of subscription.
+
+    // destroy the last node via unsub
+    obj.unsub('a', 'b', 'c', listener1);
+
+    // destroy first node in the list
+    s5.destroy();
+
+    // destroy a middle node
+    s3.destroy();
+
+    var count = obj.pub('a', 'b', 'c');
+
+    expect(count).toBe(2);
+    expect(wasCalled1).toBe(false);
+    expect(wasCalled2).toBe(true);
+    expect(wasCalled3).toBe(false);
+    expect(wasCalled4).toBe(true);
+    expect(wasCalled5).toBe(false);
+  });
+
+  // Tests for long topics, non-string topics.
+  // Provides coverage of special optimization code paths.
+  it('pub/sub special cases', function() {
+    foam.CLASS({
+      name: 'SomeClass'
+    });
+
+    var obj = SomeClass.create();
+
+    var s1;
+    var wasCalled1 = false;
+    var listener1 = function() {
+      wasCalled1 = true;
+    };
+
+    var s2;
+    var wasCalled2 = false;
+    var listener2 = function() {
+      wasCalled2 = true;
+    };
+
+    s1 = obj.sub(listener1);
+    s2 = obj.sub(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, listener2);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub();
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5, 6);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5, 6, 7);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5, 6, 7, 8);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(false);
+
+    wasCalled1 = false;
+    wasCalled2 = false;
+    obj.pub(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(true);
+
+    s1.destroy();
+    s2.destroy();
+
+
+    // Topics are automatically turned to strings.
+    var topicObj = {
+      toString: function() {
+        return 'foo';
+      }
+    };
+
+    var now = Date.now();
+    var dateNow = new Date(now);
+    var stringNow = dateNow.toString();
+
+    // Both subscription are to the same topic, since each
+    // argument evaluates to the same string.
+    obj.sub('topic', topicObj, dateNow,       listener1);
+    obj.sub('topic', 'foo',    new Date(now), listener2);
+
+    wasCalled1 = wasCalled2 = false;
+    obj.pub('topic', topicObj, new Date(now));
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(true);
+
+    wasCalled1 = wasCalled2 = false;
+    obj.pub('topic', 'foo', stringNow);
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(true);
+  });
+
+  it('pub/sub unsub during pub does not lose events', function() {
+    // A common bug in pubsub systems is losing events when
+    // someone unsubscribes while an event is being published.
+    // This is a regression test that helps ensure the FObject pub/sub
+    // doesn't lose events.
+
+
+    foam.CLASS({
+      name: 'SomeClass'
+    });
+
+    var obj = SomeClass.create();
+
+    var wasCalled1 = false;
+    var wasCalled2 = false;
+
+    obj.sub('a', function(s) {
+      s.destroy();
+      wasCalled1 = true;
+    });
+
+    obj.sub('a', function(s) {
+      s.destroy();
+      wasCalled2 = true;
+    });
+
+    obj.pub('a');
+
+    expect(wasCalled1).toBe(true);
+    expect(wasCalled2).toBe(true);
+  });
 });
