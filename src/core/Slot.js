f73d63551fe36d59bfdde4db09dec0fc32f1d2a5
Braden Shepherdson <braden.shepherdson@gmail.com>
2016-10-21 16:10:04 -0400

    Add several methods on Slots: follow, linkTo/From, mapTo/From, relateTo/From
    Test Plan: 100% coverage
    
    Reviewers: anicolao
    
    Reviewed By: anicolao
    
    Tags: #foam
    
    Differential Revision: http://codereview.cc/D1752

--- src/core/Slot.js
+++ src/core/Slot.js
@@ -24,10 +24,172 @@
 
   <ul>Types of Slots include:
     <li>PropertySlot
-    <li>ConstantSlot
+    <li>ExpressionSlot
 </ul>
 */
+foam.CLASS({
+  package: 'foam.core',
+  name: 'Slot',
 
+  methods: [
+    /**
+      Link two Slots together, setting both to other's value.
+      Returns a Destroyable which can be used to break the link.
+      After copying a value from one slot to the other, this implementation
+      then copies the value back in case the target slot rejected the value.
+    */
+    function linkFrom(s2) {
+      console.assert(s2 && this.cls_.isInstance(s2),
+          'Slot.linkFrom: argument is not a Slot');
+
+      var s1        = this;
+      var feedback1 = false;
+      var feedback2 = false;
+
+      // FUTURE(kgr): Once all slot types property set 'src', these
+      // two listeneners can be merged.
+      var l1 = function(e) {
+        if ( feedback1 ) return;
+
+        // FUTURE(braden): Switch this to foam.util.equals when defined.
+        if ( s1.get() !== s2.get() ) {
+          feedback1 = true;
+          s2.set(s1.get());
+          // FUTURE(braden): Switch this to foam.util.equals when defined.
+          if ( s1.get() !== s2.get() ) {
+            s1.set(s2.get());
+          }
+          feedback1 = false;
+        }
+      };
+
+      var l2 = function(e) {
+        if ( feedback2 ) return;
+
+        // FUTURE(braden): Switch this to foam.util.equals when defined.
+        if ( s1.get() !== s2.get() ) {
+          feedback2 = true;
+          s1.set(s2.get());
+          // FUTURE(braden): Switch this to foam.util.equals when defined.
+          if ( s1.get() !== s2.get() ) {
+            s2.set(s1.get());
+          }
+          feedback2 = false;
+        }
+      };
+
+      var sub1 = s1.sub(l1);
+      var sub2 = s2.sub(l2);
+
+      l2();
+
+      return {
+        destroy: function() {
+          sub1 && sub1.destroy();
+          sub2 && sub2.destroy();
+          sub1 = sub2 = null;
+        }
+      };
+    },
+
+    /** See linkFrom. linkTo simply reverses the arguments. */
+    function linkTo(other) {
+      return other.linkFrom(this);
+    },
+
+    /**
+      Have this Slot dynamically follow other's value.
+      Returns a Destroyable which can be used to cancel the binding.
+    */
+    function follow(other) {
+      console.assert(other && this.cls_.isInstance(other),
+          'Slot.follow requires Slot argument.');
+      var self = this;
+      var l = function() {
+        // FUTURE(braden): Switch this to foam.util.equals when defined.
+        if ( self.get() !== other.get() ) {
+          self.set(other.get());
+        }
+      };
+      l();
+      return other.sub(l);
+    },
+
+    /**
+     * Maps values from one model to another.
+     * @param f maps values from srcValue to dstValue
+     */
+    function mapFrom(other, f) {
+      console.assert(other && this.cls_.isInstance(other),
+          'Slot.mapFrom: first argument is not a Slot');
+      console.assert(typeof f === 'function',
+          'Slot.mapFrom: second argument is not a function');
+
+      var self = this;
+      var l = function() { self.set(f(other.get())); };
+      l();
+      return other.sub(l);
+    },
+
+    function mapTo(other, f) {
+      return other.mapFrom(this, f);
+    },
+
+    /**
+     * Relate to another Slot.
+     * @param other The other slot.
+     * @param f Map from this to other.
+     * @param fprime Map from other to this.
+     * @param expectUnstable Set this to true if you expect fPrime(f(x)) != x.
+     *    Otherwise an error is generated on divergence.
+     */
+    function relateTo(other, f, fPrime, expectUnstable) {
+      console.assert(other && this.cls_.isInstance(other),
+          'Slot.relateTo: first argument is not a Slot');
+      console.assert(typeof f === 'function',
+          'Slot.relateTo: second argument is not a function');
+      console.assert(typeof fPrime === 'function',
+          'Slot.relateTo: third argument is not a function');
+
+      var self     = this;
+      var feedback = false;
+      var feedbackCounter = 0;
+      var sub      = foam.core.FObject.create();
+      var l1 = function() {
+        if ( feedback ) return;
+        feedback = expectUnstable;
+        if ( ! expectUnstable && feedbackCounter > 5 ) {
+          throw 'relateTo: Unexpected divergence.';
+        }
+        feedbackCounter++;
+        other.set(f(self.get()));
+        feedback = false;
+        feedbackCounter--;
+      };
+      var l2 = function() {
+        if ( feedback ) return;
+        feedback = expectUnstable;
+        feedbackCounter++;
+        self.set(fPrime(other.get()));
+        feedback = false;
+        feedbackCounter--;
+      };
+
+      // FUTURE(braden): Once onDestroy is supported, pass these inner
+      // subscriptions to sub.onDestroy.
+      this.sub(l1);
+      other.sub(l2);
+
+      l1();
+
+      return sub;
+    },
+
+    function relateFrom(other, f, fPrime) {
+      return other.relateTo(this, fPrime, f);
+    }
+  ]
+});
 
 /**
   PropertySlot represents object properties as Slots.
@@ -37,6 +199,7 @@
 foam.CLASS({
   package: 'foam.core.internal',
   name: 'PropertySlot',
+  extends: 'foam.core.Slot',
   properties: [
     {
       name: 'obj'
@@ -56,6 +219,12 @@ foam.CLASS({
 
     function set(value) {
       return this.prop.set(this.obj, value);
+    },
+
+    function sub(l) {
+      var s = this.obj.sub('propertyChange', this.prop.name, l);
+      s.src = this;
+      return s;
     },
 
     function isDefined() {
--- test/src/core/Slot.js
+++ test/src/core/Slot.js
@@ -26,10 +26,7 @@ describe('PropertySlot', function() {
     });
 
     var obj = Abc.create({ a: 1 });
-
-    var slot = foam.core.internal.PropertySlot.create();
-    slot.obj = obj;
-    slot.prop = Abc.A;
+    var slot = obj.a$;
 
     expect(slot.get()).toBe(1);
 
@@ -42,7 +39,419 @@ describe('PropertySlot', function() {
     slot.clear();
     expect(slot.isDefined()).toBe(false);
 
-
     expect(slot.toString()).toBe('PropertySlot(Abc.a)');
+  });
+
+  it('supports follow()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    slot2.follow(slot1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.set(3);
+    expect(slot2.get()).toBe(3);
+
+    // But it's one-way.
+    slot2.set(6);
+    expect(slot2.get()).toBe(6);
+    expect(slot1.get()).toBe(3);
+
+    // Follow fails if passed a non-Slot.
+    expect(function() {
+      slot2.follow(null);
+    }).toThrow();
+    expect(function() {
+      slot2.follow(Abc.A);
+    }).toThrow();
+  });
+
+  it('supports linkFrom()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    slot2.linkFrom(slot1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.set(3);
+    expect(slot2.get()).toBe(3);
+
+    slot2.set(6);
+    expect(slot2.get()).toBe(6);
+    expect(slot1.get()).toBe(6);
+
+    // linkFrom fails if passed a non-Slot.
+    expect(function() {
+      slot2.linkFrom(null);
+    }).toThrow();
+    expect(function() {
+      slot2.linkFrom(Abc.A);
+    }).toThrow();
+  });
+
+  it('supports linkTo()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    slot2.linkTo(slot1);
+    expect(slot2.get()).toBe(2);
+    expect(slot1.get()).toBe(2);
+
+    slot1.set(3);
+    expect(slot2.get()).toBe(3);
+
+    slot2.set(6);
+    expect(slot2.get()).toBe(6);
+    expect(slot1.get()).toBe(6);
+
+    // linkTo fails if passed a non-Slot.
+    expect(function() {
+      slot2.linkTo(null);
+    }).toThrow();
+    expect(function() {
+      slot2.linkTo(Abc.A);
+    }).toThrow();
+  });
+
+  it('supports mapFrom() and mapTo()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+    var obj3 = Abc.create({ a: 3 });
+    var slot3 = obj3.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+    expect(slot3.get()).toBe(3);
+
+    slot2.mapFrom(slot1, function(x) { return -x; });
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(-1);
+
+    slot1.set(5);
+    expect(slot1.get()).toBe(5);
+    expect(slot2.get()).toBe(-5);
+
+    // Check one-way.
+    slot2.set(3);
+    expect(slot2.get()).toBe(3);
+    expect(slot1.get()).toBe(5); // Hasn't changed.
+
+
+    // Trying the other way with mapTo().
+    slot1.mapTo(slot3, function(x) { return 4 * x; });
+    expect(slot1.get()).toBe(5);
+    expect(slot3.get()).toBe(20);
+
+    slot1.set(3);
+    expect(slot1.get()).toBe(3);
+    expect(slot3.get()).toBe(12);
+
+    // But it's one-way.
+    slot3.set(6);
+    expect(slot3.get()).toBe(6);
+    expect(slot1.get()).toBe(3);
+
+    // mapFrom fails if passed a non-Slot, or non-function
+    expect(function() { slot2.mapFrom(null); }).toThrow();
+    expect(function() { slot2.mapFrom(Abc.A); }).toThrow();
+    expect(function() { slot2.mapFrom(slot3); }).toThrow();
+    expect(function() { slot2.mapFrom(slot3, 7); }).toThrow();
+    // mapTo fails if passed a non-Slot, or non-function
+    expect(function() { slot2.mapTo(null); }).toThrow();
+    expect(function() { slot2.mapTo(Abc.A); }).toThrow();
+    expect(function() { slot2.mapTo(slot3); }).toThrow();
+    expect(function() { slot2.mapTo(slot3, 7); }).toThrow();
+  });
+
+  it('supports relateFrom() and relateTo()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+    var obj3 = Abc.create({ a: 3 });
+    var slot3 = obj3.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+    expect(slot3.get()).toBe(3);
+
+    slot2.relateFrom(slot1, function(x) { return -x; },
+        function(x) { return -x; });
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(-1);
+
+    slot1.set(5);
+    expect(slot1.get()).toBe(5);
+    expect(slot2.get()).toBe(-5);
+
+    // Check two-way.
+    slot2.set(3);
+    expect(slot2.get()).toBe(3);
+    expect(slot1.get()).toBe(-3);
+
+    // Trying the other way with relateTo().
+    slot1.relateTo(slot3, function(x) { return 4 * x; },
+        function(x) { return x / 4; });
+    expect(slot1.get()).toBe(-3);
+    expect(slot3.get()).toBe(-12);
+
+    slot1.set(6);
+    expect(slot1.get()).toBe(6);
+    expect(slot3.get()).toBe(24);
+
+    // And it's two-way.
+    slot3.set(6);
+    expect(slot3.get()).toBe(6);
+    expect(slot1.get()).toBe(1.5);
+
+    // relateFrom fails if passed a non-Slot, or non-functions
+    expect(function() { slot2.relateFrom(null); }).toThrow();
+    expect(function() { slot2.relateFrom(Abc.A); }).toThrow();
+    expect(function() { slot2.relateFrom(slot3); }).toThrow();
+    expect(function() { slot2.relateFrom(slot3, 7); }).toThrow();
+    expect(function() {
+      slot2.relateFrom(slot3, function(x) { return x; }, 7);
+    }).toThrow();
+
+    // relateTo fails if passed a non-Slot, or non-function
+    expect(function() { slot2.relateTo(null); }).toThrow();
+    expect(function() { slot2.relateTo(Abc.A); }).toThrow();
+    expect(function() { slot2.relateTo(slot3); }).toThrow();
+    expect(function() { slot2.relateTo(slot3, 7); }).toThrow();
+    expect(function() {
+      slot2.relateTo(slot3, function(x) { return x; }, 7);
+    }).toThrow();
+  });
+
+  it('prevents feedback in relateTo/From', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    // Divergence should be caught.
+    expect(function() {
+      var obj1 = Abc.create({ a: 1 });
+      var obj2 = Abc.create({ a: 2 });
+      obj1.a$.relateTo(obj2.a$, function(x) { return x + 1; },
+          function(x) { return x + 1; });
+      obj1.a = 5;
+    }).toThrow('relateTo: Unexpected divergence.');
+
+    // In both directions.
+    expect(function() {
+      var obj1 = Abc.create({ a: 1 });
+      var obj2 = Abc.create({ a: 2 });
+      obj1.a$.relateFrom(obj2.a$, function(x) { return x + 1; },
+          function(x) { return x + 1; });
+      obj2.a = 4;
+    }).toThrow('relateTo: Unexpected divergence.');
+
+    // Setting expectUnstable should stop any feedback.
+    expect(function() {
+      var obj1 = Abc.create({ a: 1 });
+      var obj2 = Abc.create({ a: 2 });
+      obj1.a$.relateTo(obj2.a$, function(x) { return x + 1; },
+          function(x) { return x; }, true);
+      obj1.a = 3;
+      expect(obj2.a).toBe(4);
+    }).not.toThrow();
+
+    // And in reverse.
+    expect(function() {
+      var obj1 = Abc.create({ a: 1 });
+      var obj2 = Abc.create({ a: 2 });
+      obj1.a$.relateTo(obj2.a$, function(x) { return x + 1; },
+          function(x) { return x + 1; }, true);
+      obj2.a = 5;
+      expect(obj1.a).toBe(6);
+    }).not.toThrow();
+  });
+
+  it('supports destroy()', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 2 });
+    var slot2 = obj2.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    var sub = slot2.linkFrom(slot1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.set(3);
+    expect(slot2.get()).toBe(3);
+
+    // Now if I destroy it, it stops binding.
+    sub.destroy();
+    slot2.set(6);
+    expect(slot2.get()).toBe(6);
+    expect(slot1.get()).toBe(3);
+  });
+
+  it('prevents feedback', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    foam.CLASS({
+      name: 'Def',
+      properties: [
+        { name: 'b', adapt: function(old, nu) { return Math.ceil(nu); } }
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Def.create({ b: 2 });
+    var slot2 = obj2.b$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    var sub = slot2.linkFrom(slot1);
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.set(3.5);
+    expect(slot1.get()).toBe(4);
+    expect(slot2.get()).toBe(4);
+  });
+
+  it('correctly handles adapts overriding the value', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    foam.CLASS({
+      name: 'Def',
+      properties: [
+        { name: 'b', adapt: function(old, nu) { return nu > 20 ? 20 : nu; } }
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Def.create({ b: 2 });
+    var slot2 = obj2.b$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(2);
+
+    var sub = slot1.linkTo(slot2);
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.set(200);
+    expect(slot1.get()).toBe(20);
+    expect(slot2.get()).toBe(20);
+
+    slot1.set(400);
+    expect(slot1.get()).toBe(20);
+    expect(slot2.get()).toBe(20);
+
+    sub.destroy();
+
+    slot2.linkTo(slot1);
+
+    slot1.set(200);
+    expect(slot1.get()).toBe(20);
+    expect(slot2.get()).toBe(20);
+
+    slot1.set(400);
+    expect(slot1.get()).toBe(20);
+    expect(slot2.get()).toBe(20);
+  });
+
+  it('handles follow() when the values are identical', function() {
+    foam.CLASS({
+      name: 'Abc',
+      properties: [
+        'a'
+      ]
+    });
+
+    var obj1 = Abc.create({ a: 1 });
+    var slot1 = obj1.a$;
+    var obj2 = Abc.create({ a: 1 });
+    var slot2 = obj2.a$;
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(1);
+
+    slot1.follow(slot2);
+
+    expect(slot1.get()).toBe(1);
+    expect(slot2.get()).toBe(1);
   });
 });
