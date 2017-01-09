4c15024d999a7fece610e79496bfeee533c1578e
Jackson Stone <jacksonic@google.com>
2017-01-09 16:00:50 -0500

    Add the Class property type
    Summary: Adds the Class property, which automatically resolves string type names as class objects. Includes a change to Property.getter, to pass the property instance as an argument for use in custom getters that need their property's configuration.
    
    Test Plan: 100% coverage.
    
    Reviewers: anicolao
    
    Tags: #foam
    
    Differential Revision: http://codereview.cc/D2481

--- test/src/foam/core/property/Class.js
+++ test/src/foam/core/property/Class.js
@@ -43,19 +43,63 @@ describe('Class property', function() {
   });
 
   it('stores a given model instance', function() {
-    p.class = test.ClassTester;
-    expect(p.class).toBe(test.ClassTester);
+    p.class = global.test.ClassTester;
+    expect(p.class).toBe(global.test.ClassTester);
   });
   it('looks up a model from a string name', function() {
     p.class = 'test.ClassTester';
-    expect(p.class).toBe(test.ClassTester);
+    expect(p.class).toBe(global.test.ClassTester);
   });
   it('accepts undefined', function() {
     p.class = 'test.ClassTester';
-    expect(p.class).toBe(test.ClassTester);
+    expect(p.class).toBe(global.test.ClassTester);
 
     p.class = undefined;
     expect(p.class).toBeUndefined();
   });
+  it('handles a defined default value', function() {
+    foam.CLASS({
+      name: 'ClassTesterVal',
+      package: 'test',
+
+      properties: [
+        {
+          class: 'Class',
+          name: 'class',
+          value: 'foam.core.Property'
+        },
+      ]
+    });
+    var pv = global.test.ClassTesterVal.create(undefined, foam.__context__);
+    expect(pv.class).toBe(foam.core.Property);
+  });
+  it('handles a defined factory', function() {
+    foam.CLASS({
+      name: 'ClassTesterFac',
+      package: 'test',
+
+      properties: [
+        {
+          class: 'Class',
+          name: 'class',
+          factory: function() {
+            return 'foam.core.Listener';
+          }
+        }
+      ]
+    });
+    var pv = global.test.ClassTesterFac.create(undefined, foam.__context__);
+    expect(pv.class).toBe(foam.core.Listener);
+  });
+  it('handles a bad type', function() {
+    p.class = 'bad.type.Here';
+    expect(p.class).toBeUndefined();
+  });
+
+  it('serializes JSON with a string id', function() {
+    expect(global.test.ClassTester.CLASS.toJSON(foam.core.Method))
+      .toBe('foam.core.Method');
+  });
+
 
 });
